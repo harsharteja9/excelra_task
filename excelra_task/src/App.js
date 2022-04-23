ee17bde726df6be68e08d4d354ae4ce1e54f97a2
Karipam Sandeep <karipam.sandeep@globallogic.com>
2022-04-23 10:39:01 +0530

    Added onclick to button 1 to change the display value

--- excelra_task/src/App.js
+++ excelra_task/src/App.js
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleCounter = () => {
+    setCount(count + 1);
+  };
+  
   return (
     <div className="App">
       <div>
         <p>{count}</p>
       </div>
-      <button>1</button>
+      <button onClick={handleCounter}>1</button>
     </div>
   );
 }
