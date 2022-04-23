bf465c3ba627c0478aadddb8a79c16e130ac7882
Karipam Sandeep <karipam.sandeep@globallogic.com>
2022-04-23 10:53:53 +0530

    Added buttons from 0-9

--- excelra_task/src/App.js
+++ excelra_task/src/App.js
@@ -4,16 +4,31 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleCounter = () => {
-    setCount(count + 1);
+  const handleCounter = (el) => {
+    setCount(el);
   };
-  
+
   return (
     <div className="App">
       <div>
         <p>{count}</p>
       </div>
-      <button onClick={handleCounter}>1</button>
+      <div>
+        <button onClick={() => handleCounter(7)}>7</button>
+        <button onClick={() => handleCounter(8)}>8</button>
+        <button onClick={() => handleCounter(9)}>9</button>
+        <br />
+        <button onClick={() => handleCounter(4)}>4</button>
+        <button onClick={() => handleCounter(5)}>5</button>
+        <button onClick={() => handleCounter(6)}>6</button>
+        <br />
+        <button onClick={() => handleCounter(1)}>1</button>
+        <button onClick={() => handleCounter(2)}>2</button>
+        <button onClick={() => handleCounter(3)}>3</button>
+        <br />
+        <button onClick={() => handleCounter(0)}>0</button>
+        <br />
+      </div>
     </div>
   );
 }
